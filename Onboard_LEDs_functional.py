464eea925f47cd11a7303984d9d7e292503747fb
PHimmel <peterhimmelman@yahoo.com>
2020-11-27 00:27:14 -0500

    shifted documentation from program file into readme

--- Onboard_LEDs_functional.py
+++ Onboard_LEDs_functional.py
@@ -1,29 +1,3 @@
-"""
-notes for using the onboard leds:
-
-sys identifiers:
-led0 = green activity
-led1 = red power
-
-locations:
-main led directory = /sys/class/leds
-toggle functionality = /boot/config.txt
-
-they function primarily as system status indicators. This can lead to conflicting control over them(even when
-active status is turned off). Especially led1.
-
-Once in the 'trigger' state of programmic usability - they are effectively turned on and used by the system
-as indicators
-care must be taken when using them immediately after start-up or reboot. As they are susceptible to undesired events
-(see below: Sleep)
-
-Programs that rapidly alternate the states of these two leds must be very exact with their statements.
-They should be clearly turned on and off as needed (but without *any* redundant commands, eg. red-on, red-on).
-Otherwise they are prone to having their on/off states flipped (along with their required commands to do so).
-
-sleep between state alternations is also important, without a period of rest(minimum ~0.2 seconds)
-the brightness maybe significantly reduced, also response time and rate(failures occur)
-"""
 
 import os
 from time import sleep
@@ -64,8 +38,10 @@ def flash_leds(color, flashes, duration=None, rest=None):
         else:
             sleep(.1)
 
-#flash_leds(red_pwr, 3, duration=.15)
-#flash_leds(green_act, 3, duration=.15)
+# Basics uses:
 
-#onboard_leds(green_act, off)
-#onboard_leds(red_pwr, off) # the red seems to be inverted (at least part time)
+# flash_leds(red_pwr, 3, duration=.15)
+# flash_leds(green_act, 3, duration=.15)
+
+# onboard_leds(green_act, off)
+# onboard_leds(red_pwr, off) # the red seems to be inverted (at least part time)
--- README.md
+++ README.md
@@ -21,3 +21,29 @@ foobar.pluralize('word') # returns 'words'
 foobar.pluralize('goose') # returns 'geese'
 foobar.singularize('phenomena') # returns 'phenomenon'
 ```
+
+### Notes
+notes for using the onboard leds:
+
+sys identifiers:
+led0 = green activity
+led1 = red power
+
+locations:
+main led directory = /sys/class/leds
+toggle functionality = /boot/config.txt
+
+they function primarily as system status indicators. This can lead to conflicting control over them(even when
+active status is turned off). Especially led1.
+
+Once in the 'trigger' state of programmic usability - they are effectively turned on and used by the system
+as indicators
+care must be taken when using them immediately after start-up or reboot. As they are susceptible to undesired events
+(see below: Sleep)
+
+Programs that rapidly alternate the states of these two leds must be very exact with their statements.
+They should be clearly turned on and off as needed (but without *any* redundant commands, eg. red-on, red-on).
+Otherwise they are prone to having their on/off states flipped (along with their required commands to do so).
+
+sleep between state alternations is also important, without a period of rest(minimum ~0.2 seconds)
+the brightness maybe significantly reduced, also response time and rate(failures occur)
