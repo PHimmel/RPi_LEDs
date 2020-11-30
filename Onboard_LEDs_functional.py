1b9a99843eb94f9896f34c6217c649fe0350e8ad
PHimmel <peterhimmelman@yahoo.com>
2020-11-30 03:30:57 -0500

    added more explanations

--- Onboard_LEDs_functional.py
+++ Onboard_LEDs_functional.py
@@ -1,29 +1,34 @@
-
 import os
 from time import sleep
 
 on = 'on'
 off = 'off'
 
+# Bash commands for accessing leds
 red_pwr = '/sys/class/leds/led1/brightness'
 green_act = '/sys/class/leds/led0/brightness'
+
 
 def set_on(color):
     if color == red_pwr:
         return '0'
     return '1'
 
+
 def set_off(color):
     if color == red_pwr:
         return '1'
     return '0'
-    
+
+
 def onboard_leds(color, state):
     if state == on:
         state = set_on(color)
     else:
         state = set_off(color)
+    # MAIN PART
     return os.system('echo {} | sudo tee {} > /dev/null 2>&1'.format(state, color))
+
 
 def flash_leds(color, flashes, duration=None, rest=None):
     for i in range(flashes):
